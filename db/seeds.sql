a16553ffde06c6d37d74555a11be7a4487652767
Byron Tottaram <byron.tottaram@alumni.stonybrook.edu>
2020-02-18 15:28:08 -0500

    adding files (heroku link will be added momentarily)

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+insert into burgers_tbl(burger_name) values ('Cheese Burger');
+insert into burgers_tbl(burger_name) values ('Hamburger');
+insert into burgers_tbl(burger_name) values ('Veggie Burger');
+insert into burgers_tbl(burger_name) values ('Bacon cheese burger');
